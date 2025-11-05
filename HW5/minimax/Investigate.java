3c23bc159ef8a04f6e246a4ca623a1b4dfc784aa
bpmryan <bpm_ryand2428@yahoo.com>
2025-11-05 15:11:56 -0600

    Add code in Investigate.java to answer min, max, and draw stats

--- HW5/minimax/Investigate.java
+++ HW5/minimax/Investigate.java
@@ -2,5 +2,21 @@ public class Investigate {
     public static void main(String[] args) {
         Minimax model = new Minimax(3);
         System.out.println(model.root);
+
+        int minWins = DFS(model.root, -1);
+        int draws = DFS(model.root, 0);
+        int maxWins = DFS(model.root, 1);
+        System.out.println("min: " + minWins);
+        System.out.println("draw: " + draws);
+        System.out.println("max: " + maxWins);
+    }
+
+    public static int DFS(Vertex root, int player) {
+        if (root.children.size() <= 0)
+            return root.utility == player ? 1 : 0;
+        int count = 0;
+        for (Vertex child : root.children)
+            count += DFS(child, player);
+        return count;
     }
 }
