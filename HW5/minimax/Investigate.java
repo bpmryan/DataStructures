0d9ccd593a0bae80ca018d6fda9dc98854cce61b
bpmryan <bpm_ryand2428@yahoo.com>
2025-11-05 15:16:34 -0600

    Adjust the sout placement in lines 7-12 Investigate.java

--- HW5/minimax/Investigate.java
+++ HW5/minimax/Investigate.java
@@ -3,12 +3,13 @@ public class Investigate {
         Minimax model = new Minimax(3);
         System.out.println(model.root);
 
-        int minWins = DFS(model.root, -1);
+        
         int draws = DFS(model.root, 0);
         int maxWins = DFS(model.root, 1);
-        System.out.println("min: " + minWins);
+        int minWins = DFS(model.root, -1);
         System.out.println("draw: " + draws);
         System.out.println("max: " + maxWins);
+        System.out.println("min: " + minWins);
     }
 
     public static int DFS(Vertex root, int player) {
