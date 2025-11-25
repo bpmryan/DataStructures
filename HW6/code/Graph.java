855187eb35ca275d3d0373049e171b416e532df1
bpmryan <bpm_ryand2428@yahoo.com>
2025-11-25 15:21:54 -0600

    Final solution for HW6

--- HW6/code/Graph.java
+++ HW6/code/Graph.java
@@ -1,14 +1,50 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Graph {
     public static void main(String[] args) {
-        int[][] w = weights("../media/dijkstra1.txt");
-        Vertex[] G = new Vertex[w.length];
-        dijkstra(G, w, 0);
+        // int[][] w = weights("../media/dijkstra1.txt");
+        // Vertex[] G = new Vertex[w.length];
+        // dijkstra(G, w, 0);
 
-        for (int i = 0; i < w.length; i++) {
-            for (int j = 0; j < w[i].length; j++) {
-                System.out.printf("%3d", w[i][j]);
-            }
-            System.out.println("something");
+        // for (int i = 0; i < w.length; i++) {
+        //     for (int j = 0; j < w[i].length; j++) {
+        //         System.out.printf("%3d", w[i][j]);
+        //     }
+        //     System.out.println("something");
+        // }
+
+        // run Dijkstra
+        runDijkstra("../media/dijkstra1.txt");
+        runDijkstra("../media/dijkstra2.txt");
+
+        // run Bellman
+        runBellmanFord("../media/bellmanFord1.txt");
+        runBellmanFord("../media/bellmanFord2.txt");
+    }
+
+    public static void runDijkstra(String path) {
+        System.out.println("Dijkstra results:");
+        int[][] w = weights(path);
+        Vertex[] G = new Vertex[w.length];
+        dijkstra(G,w,0);
+
+        for (Vertex v : G) {
+            System.out.println(v.label() + ": " + v.d + " via " + v.pi);
+        }
+    }
+
+    public static void runBellmanFord(String path){
+        System.out.println("Bellman Ford results:");
+        int[][] w2 = weights(path);
+        Vertex[] G2 = new Vertex[w2.length];
+        boolean ok = bellmanFord(G2, w2, 0);
+        if(!ok) {
+            System.out.println("Negative weight detected");
+        }
+        for (Vertex v : G2) {
+            System.out.println(v.label() + ": " + v.d + " via " + v.pi);
         }
     }
 
@@ -16,11 +52,13 @@ public class Graph {
         initializeSingleSource(G, si);
         Vertex u;
         while ((u = dequeue(G)) != null) {
-            for (int j = 0; j < w[j].length; j++)
-                relax(u, G[j], w);
-
+            for (int v = 0; v < w[u.i].length; v++) {
+                // relax if the edge is positive
+                if (w[u.i][v] > 0) {
+                    relax(u, G[v], w);
+                }
+            }
         }
-        return;
     }
 
     // last thing needed to implement 
@@ -29,7 +67,55 @@ public class Graph {
             v.d = u.d + w[u.i][v.i];
             v.pi = u.label();
         }
-        return;
+    }
+
+    // implemented the same way as dijkstra 
+    public static boolean bellmanFord(Vertex[] G, int[][] w, int si) {
+        initializeSingleSource(G, si);
+        int V = G.length;
+
+        // Relaxes edges 
+        for (int k = 0; k < V - 1; k++) {
+            for (int u = 0; u < V; u++) {
+                for (int v = 0; v < V; v++) {
+                    if (w[u][v] != 0) {   // there is an edge u -> v
+                        relax(G[u], G[v], w);
+                    }
+                }
+            }
+        }
+
+        // Check negative-weights 
+        for (int u = 0; u < V; u++) {
+            for (int v = 0; v < V; v++) {
+                if(w[u][v] != 0 && G[v].d > G[u].d + w[u][v]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static int[][] weights(String path) {
+        String contents = read(path);
+
+        String[] rows = contents.split("\n");
+        int[][] ret = new int[rows.length][rows[0].split(",").length];
+
+        String[] row = null;
+        for (int i = 0; i < rows.length; i++) {
+            row = rows[i].split(",");
+            for (int j = 0; j < ret[i].length; j++) {
+                ret[i][j] = Integer.parseInt(row[j]);
+            }
+        }
+        return ret;
+    }
+
+    public static void initializeSingleSource(Vertex[] G, int si) {
+        for (int i = 0; i < G.length; i++)
+            G[i] = new Vertex(i);
+        G[si].d = 0;
     }
 
     public static Vertex dequeue(Vertex[] G) {
@@ -42,27 +128,6 @@ public class Graph {
                 ret = v;
         if (ret != null)
             ret.visited = true;
-        return ret;
-    }
-
-    public static void initializeSingleSource(Vertex[] G, int si) {
-        for (int i = 0; i < G.length; i++)
-            G[i] = new Vertex(i);
-        G[si].d = 0;
-
-    }
-
-    public static int[][] weights(String path) {
-        String contents = Frequency.read(path);
-        String[] rows = contents.split("/n");
-        int[][] ret = new int[rows.length][rows[0].split(",").length];
-        String[] row = null;
-        for (int i = 0; i < ret.length; i++) {
-            row = rows[i].split(",");
-            for (int j = 0; j < ret[i].length; j++) {
-                ret[i][j] = Integer.parseInt(row[j]);
-            }
-        }
         return ret;
     }
 
@@ -84,5 +149,16 @@ public class Graph {
         public char label() {
             return (char) (this.i + 'a');
         }
+    }
+
+    // function to read file instead of calling it from Frequency.java
+    public static String read(String path) {
+        byte[] ret = null;
+        try {
+            ret = Files.readAllBytes(Paths.get(path));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new String(ret);
     }
 }
